154159357a3b13949b9e19eb477532987892bbbe
LynareBlueven <jazzanijuarez@gmail.com>
2024-11-03 15:56:43 -0600

    Agregando Modelos Vol.2

--- prueba/inicio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def encabezado(request):
-  return render(request,"inicio/encabezado.html")
-
-def inicio(request):
-  return render(request,"inicio/inicio.html") 
